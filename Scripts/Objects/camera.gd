e4c74954f90bde8c853a4df5978e5ec2f6a49f50
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-11 10:50:05 -0400

    Implement camera functionality with map boundaries and target tracking

--- Scripts/Objects/camera.gd
+++ Scripts/Objects/camera.gd
@@ -1,4 +1,32 @@
 extends Camera2D
 
+@export var map: TileMapLayer
+@export var target: CharacterBody2D
+
+var tile_size
 
 # TODO: Implement Camera Functionality
+func _ready() -> void:
+	
+	make_current()
+	
+
+func _process(_delta: float) -> void:
+	if map == null:
+		return
+
+	if target == null:
+		return
+
+	var map_width = map.get_map_size.width()
+	var map_height = map.get_map_size().height()
+
+	var left_edge = 0
+	var right_edge = map_width * tile_size
+	var top_edge = 0
+	var bottom_edge = map_height * tile_size
+
+	clamp(position.x, left_edge, right_edge)
+	clamp(position.y, top_edge, bottom_edge)
+
+	self.position = target.global_position
