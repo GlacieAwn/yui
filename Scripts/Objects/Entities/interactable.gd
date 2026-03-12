5e71493c68f0cdbf38f9ee12d75f23504292ddd8
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-12 13:14:48 -0400

    Omit the interactable enum as it is redundant

--- Scripts/Objects/Entities/interactable.gd
+++ Scripts/Objects/Entities/interactable.gd
@@ -3,10 +3,6 @@ class_name Interactable extends Area2D
 var is_enabled: bool = true
 var can_trigger_cutscene: bool = false
 
-enum InteractableType {
-	NPC,
-	PROP
-}
 
 func on_interact() -> void:
 	pass
