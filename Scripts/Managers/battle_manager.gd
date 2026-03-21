fbe62f166a3f8e07bbc89dfdc846f0f9df0f5cf7
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-21 16:30:46 -0400

    Add battle state management boilerplate to battle_manager

--- Scripts/Managers/battle_manager.gd
+++ Scripts/Managers/battle_manager.gd
@@ -1,1 +1,26 @@
 extends Node
+
+enum BATTLE_STATES {
+	PLAYER_TURN,
+	ENEMY_TURN,
+	WIN,
+	DEFEAT
+}
+
+var state: BATTLE_STATES
+func _ready() -> void:
+	pass
+
+func update_battle() -> void:
+	match state:
+		BATTLE_STATES.PLAYER_TURN:
+			pass
+		BATTLE_STATES.ENEMY_TURN:
+			pass
+		BATTLE_STATES.WIN:
+			pass
+		BATTLE_STATES.DEFEAT: 
+			pass	
+		_:
+			pass
+	pass
