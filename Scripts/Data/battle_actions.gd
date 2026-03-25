9371128b206c91508c1ee92685f793f6cbe045ec
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-25 17:04:07 -0400

    Add Battle_Entity class and implement basic battle actions in battle_actions.gd

--- Scripts/Data/battle_actions.gd
+++ Scripts/Data/battle_actions.gd
@@ -1,3 +1,12 @@
 extends Node
 
 enum ACTIONS {ATTACK, SPECIAl, ITEMS, RUN}
+
+# Basic Battle actions. The idea is that different entities might have different base attacks, like Yui might have "Bite" that damages a single enemy, Snowflake might have "Scratch" that damages multiple at once, but has a lower damage rate, etc. 
+# Should probably be based on animal type, not random. 
+func bite(current: Battle_Entity, target: Battle_Entity) -> void:
+	target.health -= int(pow(1.5, current.level))
+
+func scratch() -> void:
+	#TODO
+	pass
---
+++ Scripts/Objects/Entities/battle_entity.gd
@@ -0,0 +1,8 @@
+class_name Battle_Entity extends Node2D
+
+var health: int
+var special: int
+var experience: int
+var level: int
+
+enum STATUS {NORMAL, DIZZY, BLIND, FIRE, FROZEN}
